34f1557a83059305dce2238f9a1ffb6534cef970
Victor Nieves <c_cc_vic@yahoo.com>
2025-07-14 21:59:59 -0400

    added animation to H1 header and button

--- js/script.js
+++ js/script.js
@@ -4,3 +4,15 @@
 
 gsap.registerPlugin(ScrollTrigger);
 
+gsap.from(".hero-right", {
+  scrollTrigger: {
+    trigger: ".hero-right",
+    start: "top 80%",
+    // markers: true // optional for debugging
+  },
+  opacity: 0,
+  x: 100,
+  duration: 1,
+  ease: "power2.out",
+  delay: 0.2
+});
