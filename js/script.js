66e7d71e875f7edc17bd257720b6ce911cf56d4c
Victor Nieves <c_cc_vic@yahoo.com>
2025-07-16 20:56:16 -0400

    added pop up gsap animation to the <h2> in dynamic section

--- js/script.js
+++ js/script.js
@@ -16,3 +16,52 @@ gsap.from(".hero-right", {
   ease: "power2.out",
   delay: 0.2
 });
+
+// Dynamic Pop up text animation //
+// gsap.from(".pop-up-text", {
+//   scrollTrigger: {
+//     trigger: ".pop-up-text",
+//     start: "top 80%",
+//   },
+//   scale: 0,
+//   opacity: 0,
+//   duration: 0.6,
+//   ease: "back.out(1.7)"
+// });
+
+gsap.fromTo(".pop-up-text",
+  {
+    scale: 0,
+    opacity: 0,
+  },
+  {
+    scrollTrigger: {
+      trigger: ".pop-up-text",
+      start: "top 80%",
+    },
+    scale: 1,
+    opacity: 1,
+    duration: 0.5,
+    ease: "back.out(1.7)",
+    onComplete: () => {
+      // Wiggle animation after pop-up finishes
+      gsap.to(".pop-up-text", {
+        scale: 1.2,
+        rotation: 5,
+        yoyo: true,
+        repeat: 1,
+        duration: 0.2,
+        ease: "power1.inOut",
+        onComplete: () => {
+          // Smoothly reset to original state
+          gsap.to(".pop-up-text", {
+            scale: 1,
+            rotation: 0,
+            duration: 0.3,
+            ease: "power2.out"
+          });
+        }
+      });
+    }
+  }
+);
